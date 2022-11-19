abd0c82fdbc917a0d073641958337bbfdcce6b14
butchman <oren@c-gol.com>
2022-11-19 19:19:29 +0200

    multiple run batch file

---
+++ runme.sh
@@ -0,0 +1,14 @@
+cd /home/oren/SSI/myIDIssuer
+npm start &
+cd /home/oren/SSI/myUVIssuer
+npm start &
+cd /home/oren/SSI/myTAIssuer
+npm start &
+cd /home/oren/SSI/myBankIssuer
+npm start &
+cd /home/oren/SSI/myMOHIssuer
+npm start &
+cd /home/oren/SSI/myMOHIssuer2
+npm start &
+cd /home/oren/SSI/myGovIDIssuer
+npm start &
